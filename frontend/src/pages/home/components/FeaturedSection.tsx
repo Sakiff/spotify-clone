8dc1917fbbde3f025743f44ad1338c84cc27e558
Sakiff05 <sakifcf-ap215@code.edu.az>
2025-02-10 16:00:34 +0400

    added play button component

--- frontend/src/pages/home/components/FeaturedSection.tsx
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -1,5 +1,6 @@
 import FeturedGridSkeleton from "@/components/skeletons/FeturedGridSkeleton";
 import { useMusicStore } from "@/stores/useMusicStore";
+import PlayButton from "./PlayButton";
 
 const FeaturedSection = () => {
   const { featuredSongs, isLoading, error } = useMusicStore();
@@ -25,6 +26,7 @@ const FeaturedSection = () => {
             <p className="font-medium truncate">{song.title}</p>
             <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
           </div>
+          <PlayButton song={song} />
         </div>
       ))}
     </div>
